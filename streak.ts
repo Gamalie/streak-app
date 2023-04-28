c4604684ecf148980bc16a0d126e2e858f3c1b3a
Belinda Florence <47244547+Gamalie@users.noreply.github.com>
2023-04-28 11:36:23 +0300

    Add files via upload
    Streak app with add functionality

---
+++ streak.ts
@@ -0,0 +1,29 @@
+interface Streak{
+    id:number,
+    date:string,
+   image: string,
+    activit:string
+}
+
+class Streak{
+    
+    async addActivity(id:number){
+        const display = await fetch("http://localhost:3000/activities")
+        const activities = await display.json() as Streak []
+        let html =''
+        for(let behavior of activities){
+                html+= ` <div class="activity">
+                <p><ion-icon name="logo-no-smoking"></ion-icon></p>
+                <p id=" days">${behavior.date}</p>
+                <p id = "activity">${behavior.activit}</p>
+            </div>`
+            }
+            
+
+            const streakApp = document.querySelector(".activity")
+            streakApp.innerHTML=html
+        }
+
+
+}
+new Streak()
